9920340274ffecc5b69c9f516e665c6609d94f2b
barzajack <116289474+barzajack@users.noreply.github.com>
2023-06-05 17:34:14 +0200

    aggiunta pagina header

--- progettopw/src/App.js
+++ progettopw/src/App.js
@@ -1,11 +1,7 @@
+import Header from "./components/header";
+
 function App() {
-  return (
-    <nav className="navbar">
-      <button>Registrati</button>
-      <button>Login</button>
-      <p>Ciao </p>
-    </nav>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ progettopw/src/components/header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <button>Registrati</button>
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Header;
