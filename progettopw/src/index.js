ead917069353d13b5dcafbb177584f2b40dfdd34
AlessandroFacciani <alex03fa@gmail.com>
2023-06-06 15:20:15 +0200

    Aggiunta la divisione delle pagine tramite path del router

--- progettopw/src/index.js
+++ progettopw/src/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import MainPage from "./MainPage";
-// import Login from "./login";
-// import Registry from "./registration";
+import MainPage from "./MainPage";
+import Login from "./login";
+import Registry from "./registration";
 import MuseumPage from "./paginamuseo";
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Paginamuseo from "./paginamuseo";
+
+const router =createBrowserRouter([
+    {
+        path:"/",
+        element: <MainPage/>
+    },{
+        path: "/login",
+        element: <Login/>
+    },{
+        path: "/registration",
+        element: <Registry/>
+    },{
+        path: "/paginamuseo",
+        element: <Paginamuseo/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <MainPage /> */}
-    {/* <Login /> */}
-    {/* <Registry /> */}
-    <MuseumPage />
-  </React.StrictMode>
+    <RouterProvider router={router} />
 );
