e976bd303cb86cd501d5251cc853f7a3865829f1
Alessandro01010 <117362946+Alessandro01010@users.noreply.github.com>
2023-06-13 09:37:29 +0200

    pagina artista

---
+++ progettopw/src/paginaartista.js
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import "../src/index.css";
+
+function ArtistPage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default ArtistPage;
