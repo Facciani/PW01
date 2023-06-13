afd65f1c2badfb24d1123443f522a7ebbf9c85d8
AlessandroFacciani <alex03fa@gmail.com>
2023-06-13 11:39:34 +0200

    Controllo se utente già loggato o meno

--- progettopw/src/paginaUtente.js
+++ progettopw/src/paginaUtente.js
@@ -1,8 +1,27 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import "../src/index.css";
+import {useEffect} from "react";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "./components/dbconfig/dbconfig";
+import {useNavigate} from "react-router-dom";
 
 const PaginaUtente = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                const email = user.email
+                console.log("email", email)
+            }else{
+                console.log("none")
+                navigate("/login")
+            }
+        })
+    },[])
+
    return (
     <>
     <Header />
